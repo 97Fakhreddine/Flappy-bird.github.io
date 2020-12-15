2b0ae8a4568102557f318be17d7c0b1913ad857a
97Fakhreddine <fakhri19971998@gmail.com>
2020-12-15 23:10:48 +0100

    finished

---
+++ script.js
@@ -0,0 +1,58 @@
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+var jumping = 0;
+var counter = 0;
+hole.addEventListener("animationiteration", () => {
+  var fuck = Math.random() * 3;
+  var top = fuck * 100 + 150;
+  hole.style.top = -top + "px";
+  counter++;
+});
+
+setInterval(function () {
+  var characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  if (jumping == 0) {
+    character.style.top = characterTop + 3 + "px";
+  }
+  var blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+  var characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  var cTop = -(500 - characterTop);
+
+  if (
+    characterTop > 480 ||
+    (blockLeft < 20 &&
+      blockLeft > -50 &&
+      (cTop < holeTop || cTop > holeTop + 130))
+  ) {
+    alert("Game Over. your Score : " + counter);
+    character.style.top = 100 + "px";
+    counter = 0;
+  }
+}, 10);
+
+function jump() {
+  jumping = 1;
+  let jumpCount = 0;
+  var jumpInterval = setInterval(function () {
+    var characterTop = parseInt(
+      window.getComputedStyle(character).getPropertyValue("top")
+    );
+    if (characterTop > 6 && jumpCount > 15) {
+      character.style.top = characterTop - 9 + "px";
+    }
+    if (jumpCount > 20) {
+      clearInterval(jumpInterval);
+      jumping = 0;
+      jumpCount = 0;
+    }
+    jumpCount++;
+  }, 10);
+}
